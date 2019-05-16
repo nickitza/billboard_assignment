6990d7a49c2a4a07ae3275585ce00340d1d705aa
Nicki Nixon <nickitza@gmail.com>
2019-05-16 16:19:48 -0600

    add artist seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
 Billboard.create(name: "Top 10 Classic Jazz Songs")
+
+Artist.create(name: "Miles Davis")
+Artist.create(name: "Duke Ellington")
+Artist.create(name: "John Coltrane")
+Artist.create(name: "Thelonius Monk")
